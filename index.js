e8c9c4762302ee7332b2de736f8c7146196fc952
Manik <tonysins43@gmail.com>
2023-04-18 20:24:55 +0600

    make categories json file

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const categories = require('./data/categories.json')
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('dragon in running')
+})
+
+app.get('/categories',(req,res)=>{
+    res.send(categories)
+})
+
+app.listen(port,()=>{
+    console.log(`Dragon api is running on :${port}` )
+})
